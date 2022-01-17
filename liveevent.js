7cd0032a4d286db46679a244fbbd485bbc12d6a5
Tomasz Wojciechowski <tomasz@extremedevteam.pl>
2022-01-17 19:05:33 +0100

    trying to fix apply on undefined

--- liveevent.js
+++ liveevent.js
@@ -226,58 +226,58 @@ var Liveevent;
             // Connect to the socket.
             if (!this.socket) {
                 this.socket = Extension.io.connect(url, { forceNew: true });
-                this.socket.on('liveEventStatus', function (data) {
-                    _this.liveStatusEventHandler(data, _this.globalOpts);
-                });
-                this.socket.on('connect', function () {
-                    _this.socket.emit('getStatus', { liveEventId: _this.globalOpts.id });
-                });
-                this.socket.on('disconnect', function () {
-                    _this.initSocket(_this.globalOpts);
-                });
-                this.socket.on('error', function (res) {
-                    console.warn('[ Liveevent:Socket ] Error: ' + res);
-                });
-                this.socket.on('reconnecting', function () {
-                    console.warn('[ Liveevent:Socket ] Reconnecting');
-                });
-                this.socket.on('reconnect_failed', function () {
-                    console.warn('[ Liveevent:Socket ] Reconnect failed');
-                });
-                this.socket.on('reconnect', function () {
-                    _this.socket.emit('getStatus', { liveEventId: _this.globalOpts.id });
-                });
-                this.socket.on('displayType', function (data) {
-                    // Run callback
-                    if (_this.globalOpts.callback.displayTypeUpdate) {
-                        _this.globalOpts.callback.displayTypeUpdate(data);
-                    }
-                });
-                this.socket.on('rateItQuestionStatus', function (data) {
-                    _this.currentEngageform.current.updateAnswers(data);
-                });
-                this.socket.on('multipleChoiceQuestionAnswers', function (data) {
-                    _this.currentEngageform.current.updateAnswers(data);
-                });
-                // Buzzer listening
-                this.socket.on('buzzerQuestionStatus', function (data) {
-                    // Run callback
-                    if (_this.globalOpts.callback.buzzerQuestionStatus) {
-                        data.id = opts.id;
-                        _this.globalOpts.callback.buzzerQuestionStatus(data);
-                    }
-                });
-                // Active User Count listening
-                this.socket.on('activeUserCount', function (data) {
-                    // Run callback
-                    if (_this.globalOpts.callback.activeUserCount) {
-                        _this.globalOpts.callback.activeUserCount(data);
-                    }
-                });
             }
             else {
                 this.socket.io.connect(url, { forceNew: true });
             }
+            this.socket.on('liveEventStatus', function (data) {
+                _this.liveStatusEventHandler(data, _this.globalOpts);
+            });
+            this.socket.on('connect', function () {
+                _this.socket.emit('getStatus', { liveEventId: _this.globalOpts.id });
+            });
+            this.socket.on('disconnect', function () {
+                _this.initSocket(_this.globalOpts);
+            });
+            this.socket.on('error', function (res) {
+                console.warn('[ Liveevent:Socket ] Error: ' + res);
+            });
+            this.socket.on('reconnecting', function () {
+                console.warn('[ Liveevent:Socket ] Reconnecting');
+            });
+            this.socket.on('reconnect_failed', function () {
+                console.warn('[ Liveevent:Socket ] Reconnect failed');
+            });
+            this.socket.on('reconnect', function () {
+                _this.socket.emit('getStatus', { liveEventId: _this.globalOpts.id });
+            });
+            this.socket.on('displayType', function (data) {
+                // Run callback
+                if (_this.globalOpts.callback.displayTypeUpdate) {
+                    _this.globalOpts.callback.displayTypeUpdate(data);
+                }
+            });
+            this.socket.on('rateItQuestionStatus', function (data) {
+                _this.currentEngageform.current.updateAnswers(data);
+            });
+            this.socket.on('multipleChoiceQuestionAnswers', function (data) {
+                _this.currentEngageform.current.updateAnswers(data);
+            });
+            // Buzzer listening
+            this.socket.on('buzzerQuestionStatus', function (data) {
+                // Run callback
+                if (_this.globalOpts.callback.buzzerQuestionStatus) {
+                    data.id = opts.id;
+                    _this.globalOpts.callback.buzzerQuestionStatus(data);
+                }
+            });
+            // Active User Count listening
+            this.socket.on('activeUserCount', function (data) {
+                // Run callback
+                if (_this.globalOpts.callback.activeUserCount) {
+                    _this.globalOpts.callback.activeUserCount(data);
+                }
+            });
         };
         // Get Liveevent
         Liveevent.prototype.getById = function (id) {
@@ -465,61 +465,61 @@ var ChatModule;
             var url = Extension.config.backend.socket;
             if (!this.socket) {
                 this.socket = Extension.io.connect(url, { forceNew: true });
-                this.socket.on('error', function (res) {
-                    console.warn(res);
-                });
-                this.socket.on('connect', function (data) {
-                    // Join room
-                    _this.socket.emit('joinRoom', _this.id);
-                    // We can also leave room, to do so just emit 'leaveRoom' with roomId as param
-                });
-                // New msg event
-                this.socket.on('msg', function (data) {
-                    // "msg" event is triggered not only when new message arrives, but also a message changes.
-                    var existingMsg = _.find(_this.messages, function (message) {
-                        return message.id === data.id;
-                    });
-                    var existingUser = _.find(_this.messages, function (message) {
-                        return message.userName === data.userName;
-                    });
-                    data.avatarColor = existingUser ? existingUser.avatarColor : _this.getRandomColor();
-                    Extension.$rootScope.$apply(function () {
-                        if (existingMsg) {
-                            _this.handleNewMessageData(existingMsg, data);
-                        }
-                        else {
-                            if (_this.direction && _this.direction === 'ttb') {
-                                _this.messages.push(data);
-                            }
-                            else {
-                                _this.messages.unshift(data);
-                            }
-                        }
-                    });
-                    if (!existingMsg) {
-                        _this._liveevent.event.trigger('chat::message', _this._liveevent.id, data);
-                    }
-                });
-                this.socket.on('msgHide', function (id) {
-                    _this._liveevent.event.trigger('chat::hideMessage', id);
-                    var messageIndex = _this.messages.length;
-                    for (var i = 0; i < _this.messages.length; i += 1) {
-                        if (_this.messages[i].id === id) {
-                            messageIndex = i;
-                        }
-                    }
-                    Extension.$rootScope.$apply(function () {
-                        _this.messages.splice(messageIndex, 1);
-                    });
-                });
-                // On disconect
-                this.socket.on('disconnect', function () {
-                    _this.initSocket();
-                });
             }
             else {
                 this.socket.io.connect();
             }
+            this.socket.on('error', function (res) {
+                console.warn(res);
+            });
+            this.socket.on('connect', function (data) {
+                // Join room
+                _this.socket.emit('joinRoom', _this.id);
+                // We can also leave room, to do so just emit 'leaveRoom' with roomId as param
+            });
+            // New msg event
+            this.socket.on('msg', function (data) {
+                // "msg" event is triggered not only when new message arrives, but also a message changes.
+                var existingMsg = _.find(_this.messages, function (message) {
+                    return message.id === data.id;
+                });
+                var existingUser = _.find(_this.messages, function (message) {
+                    return message.userName === data.userName;
+                });
+                data.avatarColor = existingUser ? existingUser.avatarColor : _this.getRandomColor();
+                Extension.$rootScope.$apply(function () {
+                    if (existingMsg) {
+                        _this.handleNewMessageData(existingMsg, data);
+                    }
+                    else {
+                        if (_this.direction && _this.direction === 'ttb') {
+                            _this.messages.push(data);
+                        }
+                        else {
+                            _this.messages.unshift(data);
+                        }
+                    }
+                });
+                if (!existingMsg) {
+                    _this._liveevent.event.trigger('chat::message', _this._liveevent.id, data);
+                }
+            });
+            this.socket.on('msgHide', function (id) {
+                _this._liveevent.event.trigger('chat::hideMessage', id);
+                var messageIndex = _this.messages.length;
+                for (var i = 0; i < _this.messages.length; i += 1) {
+                    if (_this.messages[i].id === id) {
+                        messageIndex = i;
+                    }
+                }
+                Extension.$rootScope.$apply(function () {
+                    _this.messages.splice(messageIndex, 1);
+                });
+            });
+            // On disconect
+            this.socket.on('disconnect', function () {
+                _this.initSocket();
+            });
         };
         Chat.prototype.init = function () {
             var _this = this;
